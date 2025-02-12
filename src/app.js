495825c7dbcc09eccd09657a35ac53b5421f9300
Muniz Neto <muniznetto@outlook.com>
2025-02-12 12:48:28 -0300

    add  para liberar acesso.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import routers from "./router/index.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 routers(app);
 
