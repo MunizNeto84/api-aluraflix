83865a911dc69354cd4f1129cdee13e27157fc50
Muniz Neto <muniznetto@outlook.com>
2025-02-12 12:10:32 -0300

    criando rota  canal.

--- src/router/canalRouter.js
+++ src/router/canalRouter.js
@@ -5,6 +5,9 @@ import authMiddleware from "../middleware/AuthMiddleware.js";
 const canalController = new CanalController();
 const routers = Router();
 
+routers.get("/api/v1/canal/:id", authMiddleware, (req, res, next) =>
+  canalController.getById(req, res, next)
+);
 routers.post("/api/v1/canal", authMiddleware, (req, res, next) =>
   canalController.create(req, res, next)
 );
