9da212ce5ae9c5567ebb63f2bce231f38662783f
Muniz Neto <muniznetto@outlook.com>
2025-02-16 20:52:32 -0300

    criado  canal.

--- src/router/canalRouter.js
+++ src/router/canalRouter.js
@@ -5,9 +5,14 @@ import authMiddleware from "../middleware/AuthMiddleware.js";
 const canalController = new CanalController();
 const routers = Router();
 
+routers.get("/api/v1/canal/", authMiddleware, (req, res, next) =>
+  canalController.getAll(req, res, next)
+);
+
 routers.get("/api/v1/canal/:id", authMiddleware, (req, res, next) =>
   canalController.getById(req, res, next)
 );
+
 routers.post("/api/v1/canal", authMiddleware, (req, res, next) =>
   canalController.create(req, res, next)
 );
